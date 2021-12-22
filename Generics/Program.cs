23328c03bb81994212bd69166f5f897d0cd96cd5
Leascry <Leascry@DESKTOP-MLBBGTU>
2021-12-22 15:49:22 +0300

    Proje dosyası ekle.

---
+++ Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> yasAd = new MyDictionary<int, string>();
+            yasAd.Add(18, "Emirhan");
+            yasAd.Add(21, "Oğuzhan");
+            yasAd.Add(24, "Ulaş");
+            yasAd.Add(16, "Ebrar");
+
+        }
+    }
+}
